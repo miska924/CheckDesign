671e668ef66c294408ff137148bde0a5a0ca5862
Mike Gudz <miska924code@gmail.com>
2023-04-01 00:52:03 +0300

    space & tabular begin

--- src/__init__.py
+++ src/__init__.py
@@ -1,62 +1,67 @@
-from PIL import Image, ImageEnhance
-from PIL import ImageFilter
+import logging
+import uuid
+import tempfile
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import Application, MessageHandler, filters
+
+from PIL import Image
 
 from tqdm import tqdm
 
-# import numpy as np
-# import cv2
-
-def main():
-    source_image = Image.open("resources/example3.jpg").convert('RGB')
-    gray = source_image.convert('L')
-    
-    w, h = gray.size
-
-    w_tmp_1 = 100
-    h_tmp_1 = h * w_tmp_1 // w
-
-    count = min(w, h) // 5 // 10
-    kernel_size = 3
-    rank = 3 * 3 - 2
-
-    print(kernel_size)
-
-    tmp = gray.filter(
-        ImageFilter.FIND_EDGES
-    )
+from criteria import space, tabular
 
 
-    for w_tmp in [1000, 500, 100, 50, 30]:
-        h_tmp = h * w_tmp // w
-        tmp = tmp.filter(ImageFilter.RankFilter(kernel_size, rank))
-        tmp = ImageEnhance.Contrast(tmp).enhance(256)
-        tmp = tmp.resize(size=[w_tmp, h_tmp])
-        tmp = tmp.resize(size=[w, h], resample=Image.Resampling.NEAREST)
-        # tmp = ImageEnhance.Contrast(tmp).enhance(127)
-        tmp.show()
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-    # tmp = ImageEnhance.Contrast(tmp).enhance(256)
-    tmp = tmp.resize(size=[w, h], resample=Image.Resampling.NEAREST)
+async def downloader(update, context):
+    source = None
+    with tempfile.NamedTemporaryFile() as tmp_in, tempfile.NamedTemporaryFile() as tmp_out:
+        file = await context.bot.get_file(update.message.document)
+        await file.download_to_drive(tmp_in.name)
 
-        # tmp = ImageEnhance.Contrast(tmp).enhance(2)
+        source = Image.open(tmp_in).convert('RGB')
+
+        space_rate, space_mask = space(source)
+
+        Image.blend(source, space_mask, alpha=0.7).save(tmp_out.name, "JPEG")
+        
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=tmp_out.name)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{int(space_rate * 100)}% of space")
 
 
-    # tmp = ImageEnhance.Contrast(tmp).enhance(256)
+        tabular_check, tabular_mask = tabular(source)
+        Image.blend(source, tabular_mask, alpha=0.7).save(tmp_out.name, "JPEG")
 
-
-    tmp = tmp.resize(size=[w, h], resample=Image.Resampling.NEAREST)
-
-    # got = small.filter(
-    #     # ImageFilter.MaxFilter(kernel_size)
-    #     ImageFilter.RankFilter(kernel_size, rank)
-    # ).resize
-
-    # tmp = ImageEnhance.Contrast(tmp).enhance(256)
-
-    tmp = Image.blend(source_image, tmp.convert('RGB'), alpha=.4)
-
-    tmp.show()
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=tmp_out.name)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Tabular {'OK' if tabular_check else 'FAIL'}")
     
 
-if __name__ == "__main__":
-    main()
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="test")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    token = None
+    with open('resources/token', 'r') as f:
+        token = f.read()
+
+    application = ApplicationBuilder().token(token).build()
+    
+    start_handler = CommandHandler('start', start)
+    # test_handler = CommandHandler('test', test)
+
+    message_handler = MessageHandler(filters.Document.ALL, downloader)
+
+    application.add_handler(start_handler)
+    # application.add_handler(test_handler)
+    application.add_handler(message_handler)
+    
+    application.run_polling()
