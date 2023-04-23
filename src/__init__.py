78dec92c71f9112a02ccd01510e4018ed5469217
Mike Gudz <miska924code@gmail.com>
2023-04-23 13:26:23 +0300

    fix

--- src/__init__.py
+++ src/__init__.py
@@ -76,7 +76,7 @@ async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+        chat_id=update.effective_chat.id, text="Отправь мне баннер, а я его проверю"
     )
 
 
